2347b436fcb0c4e2dbd1a6026459255e94d4838e
Kai Williams <154692413+ChiWilliams@users.noreply.github.com>
2024-11-26 12:58:09 -0500

    Fixed CORS error

--- main.py
+++ main.py
@@ -39,8 +39,8 @@ app = FastAPI(lifespan=lifespan)
 
 origins = [
     "http://localhost",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
     "http://localhost:8080",
     "http://127.0.0.1:8080"
 ]
