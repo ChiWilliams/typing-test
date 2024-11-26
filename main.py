38654e160e078ae1d9f314aadcc148cda8820363
Kai Williams <154692413+ChiWilliams@users.noreply.github.com>
2024-11-26 15:45:36 -0500

    Added kai-williams.com to the CORS allowed origins

--- main.py
+++ main.py
@@ -42,7 +42,8 @@ origins = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://localhost:8080",
-    "http://127.0.0.1:8080"
+    "http://127.0.0.1:8080",
+    "https://kai-williams.com/"
 ]
 
 
